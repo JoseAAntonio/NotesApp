5515465009a4e5cb610325b6dbef447d299f0ced
José Antônio <antoniojaraujo0@gmail.com>
2025-04-22 21:15:40 -0300

    added Auth Context and Provider

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,40 +1,44 @@
 import { Stack } from "expo-router";
+import { AuthProvider } from "../contexts/AuthContext";
 
 const RootLayout = () => {
-  return <Stack 
-    screenOptions={{
-      headerStyle: {
-        backgroundColor: '#ff8c00',
-        
-      },
-      headerTintColor: '#fff',
-      headerTitle: {
-        fontSize: 20,
-        fontWeight: 'bold',
-      },
-      headerTitleAlign: 'center',
-      contentStyle: {
-        paddingHorizontal: 10,
-        paddingTop: 10,
-        backgroundColor: 'fff'
-      },
-      headerSafeAreaInsets: { top: 0 },
-      statusBarStyle: 'light'
-    }}
-  >
-    <Stack.Screen
-      name="index"
-      options={{
-        title: "Home"
-      }}
-    />
-    <Stack.Screen
-      name="notes"
-      options={{
-      headerTitle : "Notes"
-      }}
-    />
-  </Stack>;
-}
+  return (
+    <AuthProvider>
+      <Stack
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#ff8c00",
+          },
+          headerTintColor: "#fff",
+          headerTitle: {
+            fontSize: 20,
+            fontWeight: "bold",
+          },
+          headerTitleAlign: "center",
+          contentStyle: {
+            paddingHorizontal: 10,
+            paddingTop: 10,
+            backgroundColor: "fff",
+          },
+          headerSafeAreaInsets: { top: 0 },
+          statusBarStyle: "light",
+        }}
+      >
+        <Stack.Screen
+          name="index"
+          options={{
+            title: "Home",
+          }}
+        />
+        <Stack.Screen
+          name="notes"
+          options={{
+            headerTitle: "Notes",
+          }}
+        />
+      </Stack>
+    </AuthProvider>
+  );
+};
 
 export default RootLayout;
